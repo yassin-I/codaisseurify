a48e397390fd5b5257fd9dd04ab7ed043fde287d
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-08 22:03:46 +0200

    One-to-many: Models

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+  def self.order_by_name
+  order(:music_genre)
+  end
+  belongs_to :artist
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+Artist.delete_all
+
+artist1 = artist.create!(name: "Snoop")
+artist2 = artist.create!(name: "James")
+artist3 = artist.create!(name: "Afro")
+
+artists_count = artist.all.length
+puts "#{artists_count} artists were created."
+
+Song.create!([
+  { name: "Iguanodon", age: 3, image_url: "https://goo.gl/i0S0iv", artist: artist1 },
+  { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/MYf6Sc", artist: artist2 },
+  { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/bS5SdS", artist: artist3 },
+])
+
+songs_count = Dinosaur.all.length
+puts "#{songs_count} songs were created."
