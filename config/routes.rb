e2799d9bfd57952dae1260d3e9c4d9d73f08ee03
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-09 01:57:49 +0200

    Overview list of songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :visitors
   resources :artists
   root to: 'pages#home'
   get 'pages/home'
 
-  devise_for :visitors
+
+
   get "songs" => "songs#index"
+  get "songs/:id" => "songs#show", as: :song
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
