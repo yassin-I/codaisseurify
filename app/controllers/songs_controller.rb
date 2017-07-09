5b812460d31bddc095510718530fe5112b2f433c
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-09 03:29:21 +0200

    Create show and new song pages

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,4 +5,21 @@ class SongsController < ApplicationController
   def show
   @song = Song.find(params[:id])
   end
+  def new
+    @song = Song.new
+  end
+  def create
+  song_params = params.require(:song).permit(:music_genre, :producer_name, :image_url)
+
+  @song = song.new(song_params)
+
+  if @song.save
+     redirect_to @song
+  else
+     render 'new'
+  end
+  end
+  def edit
+  @song = Song.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
 
   get "songs" => "songs#index"
+  get 'songs/new' => "songs#new", as: :new_song
   get "songs/:id" => "songs#show", as: :song
+  post "songs" => "songs#create"
+  get 'songs/:id/edit' => 'songs#edit', as: :edit_song
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
